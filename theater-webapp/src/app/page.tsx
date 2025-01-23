f036efab500a3bb5e0a45af259db6b012e236b83
Jairoalperez <jairoalberto98@hotmail.com>
2025-01-23 09:45:53 -0500

    created Hello World home page

---
+++ theater-webapp/src/app/page.tsx
@@ -0,0 +1,13 @@
+import React, {useEffect, useState} from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <h1>
+        Hello World!
+      </h1>
+    </div>
+  )
+}
+
+export default Home;
