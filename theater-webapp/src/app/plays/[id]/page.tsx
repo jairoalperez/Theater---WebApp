6ff06e187bde4f1ae5fc3544ec3721ab130a6535
Jairoalperez <jairoalberto98@hotmail.com>
2025-01-28 11:15:41 -0500

    created base for plays page

---
+++ theater-webapp/src/app/plays/[id]/page.tsx
@@ -0,0 +1,64 @@
+"use client"
+
+import React from "react"
+import { useParams } from "next/navigation"
+import axios from "axios"
+import { cn } from "@/lib/utils"
+import { Play } from "@/types/play"
+import { LoaderPinwheelIcon } from "@/components/ui/loader-pinwheel"
+import { Sleep } from "@/helpers/sleep"
+import Link from "next/link"
+
+const PlayPage: React.FC = () => {
+	const params = useParams()
+	const id = params?.id
+
+	const [play, setPlay] = React.useState<Play | null>(null)
+	const [loading, setLoading] = React.useState<boolean>(true)
+	const [error, setError] = React.useState<string | null>(null)
+
+	React.useEffect(() => {
+		if (id) {
+			let config = {
+				method: "get",
+				url: `https://localhost:7112/api/plays/${id}`,
+			}
+
+			axios
+				.request(config)
+				.then(async (response) => {
+					setPlay(response.data)
+					await Sleep(500)
+					setLoading(false)
+				})
+				.catch(async (error) => {
+					setError(`${error.status} Error fetching data: ${error.response.data}`)
+					await Sleep(500)
+					setLoading(false)
+				})
+		} else {
+			setError(`No id`)
+			setLoading(false)
+		}
+	}, [id])
+
+	return (
+		<div className="flex justify-center">
+			<div className={cn("mx-auto flex flex-col items-center p-4")}>
+				{loading ? (
+					<div className="h-56 w-full object-cover object-end flex items-center justify-center">
+						<LoaderPinwheelIcon isAnimating={true} />
+					</div>
+				) : error ? (
+					<div className="h-56 w-full object-cover object-end flex items-center justify-center">
+						<div className="text-foreground">{error}</div>
+					</div>
+				) : (
+					<div>{play?.title}</div>
+				)}
+			</div>
+		</div>
+	)
+}
+
+export default PlayPage
