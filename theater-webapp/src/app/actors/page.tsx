c6a0c13d4f6967457aebac508a5b963d949c5d4f
Jairoalperez <jairoalberto98@hotmail.com>
2025-02-20 13:18:45 -0500

    added modal to actors page

--- theater-webapp/src/app/actors/page.tsx
+++ theater-webapp/src/app/actors/page.tsx
@@ -10,7 +10,9 @@ import Link from "next/link"
 import { LoaderPinwheelIcon } from "@/components/ui/loader-pinwheel"
 import { Sleep } from "@/helpers/sleep"
 import * as dotenv from "dotenv"
-dotenv.config();
+import { MapPinPlusInsideIcon } from "@/components/ui/map-pin-plus-inside"
+import CreateActorModal from "@/components/Actors/CreateActorModal"
+dotenv.config()
 const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL
 
 const ActorsList: React.FC = () => {
@@ -19,7 +21,7 @@ const ActorsList: React.FC = () => {
 	const [loading, setLoading] = React.useState<boolean>(true)
 	const [error, setError] = React.useState<string | null>(null)
 
-	const [isModalOpen, setIsModalOpen] = React.useState(false)
+	const [isCreateModalOpen, setIsCreateModalOpen] = React.useState(false)
 
 	React.useEffect(() => {
 		let config = {
@@ -40,7 +42,7 @@ const ActorsList: React.FC = () => {
 				await Sleep(500)
 				setLoading(false)
 			})
-	}, [])
+	}, [isCreateModalOpen])
 
 	return loading ? (
 		<div className="flex justify-center">
@@ -54,7 +56,7 @@ const ActorsList: React.FC = () => {
 	) : error ? (
 		<div className="flex justify-center">
 			<div className={cn("mx-auto flex flex-col items-center p-4")}>
-                <h1 className="text-4xl font-bold mx-auto mb-8 mt-8 text-foreground">Actors</h1>
+				<h1 className="text-4xl font-bold mx-auto mb-8 mt-8 text-foreground">Actors</h1>
 				<div className="h-56 w-full object-cover object-end flex items-center justify-center">
 					<div className="text-foreground">{error}</div>
 				</div>
@@ -63,7 +65,12 @@ const ActorsList: React.FC = () => {
 	) : (
 		<div className="flex justify-center pb-16">
 			<div className={cn("mx-auto flex flex-col items-center")}>
-				<h1 className="text-4xl font-bold mx-auto mb-8 mt-8 text-foreground">Actors</h1>
+				<div className="mx-auto mb-8 mt-8 flex flex-row items-center">
+					<h1 className="text-4xl font-bold text-foreground">Actors</h1>
+					<button className="ml-8" onClick={() => setIsCreateModalOpen(true)}>
+						<MapPinPlusInsideIcon />
+					</button>
+				</div>
 				<div
 					className={cn(
 						"grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-4 p-4"
@@ -76,6 +83,7 @@ const ActorsList: React.FC = () => {
 					))}
 				</div>
 			</div>
+			<CreateActorModal isOpen={isCreateModalOpen} onClose={() => setIsCreateModalOpen(false)} />
 		</div>
 	)
 }
