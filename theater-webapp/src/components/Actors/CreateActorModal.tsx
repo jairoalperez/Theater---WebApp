8837373edd2aa2ed92d9322a5844c92f46f2a984
Jairoalperez <jairoalberto98@hotmail.com>
2025-02-20 16:14:09 -0500

    worked on create actor modal

--- theater-webapp/src/components/Actors/CreateActorModal.tsx
+++ theater-webapp/src/components/Actors/CreateActorModal.tsx
@@ -5,24 +5,15 @@ import axios from "axios"
 import { Button } from "../ui/button"
 import { Input } from "../ui/input"
 import { XIcon } from "../ui/x"
-import { Card, CardContent, CardHeader, CardTitle } from "../ui/card"
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "../ui/card"
+import { Select } from "@radix-ui/react-select"
+import { SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select"
+import { genders, eyeColors, skinColors, hairs } from "../../data/actorAtributes"
 
 interface CreateActorModalProps {
 	isOpen: boolean
 	onClose: () => void
 }
-
-// {
-//     "firstName": "string",
-//     "lastName": "string",
-//     "dob": "string",
-//     "gender": "string",
-//     "skinColor": "string",
-//     "eyeColor": "string",
-//     "hairColor": "string",
-//     "frontImage": "string",
-//     "fullBodyImage": "string"
-//   }
 
 const CreateActorModal: React.FC<CreateActorModalProps> = ({ isOpen, onClose }) => {
 	const [firstName, setFirstName] = React.useState("")
@@ -59,7 +50,7 @@ const CreateActorModal: React.FC<CreateActorModalProps> = ({ isOpen, onClose }) 
 		}
 	}
 
-	return isOpen ? null : (
+	return !isOpen ? null : (
 		<div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
 			<Card>
 				<CardHeader>
@@ -75,17 +66,72 @@ const CreateActorModal: React.FC<CreateActorModalProps> = ({ isOpen, onClose }) 
 							value={firstName}
 							onChange={(e) => setFirstName(e.target.value)}
 							required
-							className="border border-gray-300 p-2 mb-4 w-full"
+							className="border p-2 mb-4 w-full"
 						/>
-                        <Input
-                            type="text"
-                            placeholder="Last Name"
-                            value={lastName}
-                            onChange={(e) => setLastName(e.target.value)}
-                            required
-                            className="border border-gray-300 p-2 mb-4 w-full"
-                        />
+						<Input
+							type="text"
+							placeholder="Last Name"
+							value={lastName}
+							onChange={(e) => setLastName(e.target.value)}
+							required
+							className="border p-2 mb-4 w-full"
+						/>
+						<Select value={gender} onValueChange={setGender}>
+							<SelectTrigger className="border p-2 mb-4 w-full">
+								<SelectValue placeholder="Gender" />
+							</SelectTrigger>
+							<SelectContent>
+								{genders.map((val, key) => (
+									<SelectItem key={key} value={val}>{val}</SelectItem>
+								))}
+							</SelectContent>
+						</Select>
+						<Select value={skinColor} onValueChange={setSkinColor}>
+							<SelectTrigger className="border p-2 mb-4 w-full">
+								<SelectValue placeholder="Skin" />
+							</SelectTrigger>
+							<SelectContent>
+								{skinColors.map((val, key) => (
+									<SelectItem key={key} value={val}>{val}</SelectItem>
+								))}
+							</SelectContent>
+						</Select>
+						<Select value={eyeColor} onValueChange={setEyeColor}>
+							<SelectTrigger className="border p-2 mb-4 w-full">
+								<SelectValue placeholder="Eyes" />
+							</SelectTrigger>
+							<SelectContent>
+								{eyeColors.map((val, key) => (
+									<SelectItem key={key} value={val}>{val}</SelectItem>
+								))}
+							</SelectContent>
+						</Select>
+						<Select value={hairColor} onValueChange={setHairColor}>
+							<SelectTrigger className="border p-2 mb-4 w-full">
+								<SelectValue placeholder="Hair" />
+							</SelectTrigger>
+							<SelectContent>
+								{hairs.map((val, key) => (
+									<SelectItem key={key} value={val}>{val}</SelectItem>
+								))}
+							</SelectContent>
+						</Select>
 					</CardContent>
+					<CardFooter className="flex justify-between">
+						<Button
+							onClick={onClose}
+							variant="outline"
+							className="bg-red-500 bg-opacity-50 font-bold hover:bg-red-500 hover:bg-opacity-100 hover:text-background"
+						>
+							Close
+						</Button>
+						<Button
+							type="submit"
+							className="bg-yellow-500 bg-opacity-50 font-bold text-foreground hover:text-background"
+						>
+							Create
+						</Button>
+					</CardFooter>
 				</form>
 			</Card>
 		</div>
