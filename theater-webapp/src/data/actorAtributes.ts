c892346dcda46cbfa5ab7ec5cac70c7b48795f4d
Jairoalperez <jairoalberto98@hotmail.com>
2025-02-20 16:14:33 -0500

    created the file with the attributes for the selects on actor creation

---
+++ theater-webapp/src/data/actorAtributes.ts
@@ -0,0 +1,31 @@
+export const genders: string[] = ["Male", "Female", "Other"]
+
+export const skinColors: string[] = [
+    "White",
+    "Light Brown",
+    "Brown",
+    "Dark Brown",
+    "Black"
+]
+
+export const eyeColors: string[] = [
+    "Light Brown",
+    "Brown",
+    "Dark Brown",
+    "Yellow",
+    "Green",
+    "Blue",
+    "Gray"
+]
+
+export const hairs: string[] = [
+    "Light Brown",
+    "Brown",
+    "Dark Brown",
+    "Light Blonde",
+    "Blonde",
+    "Dark Blonde",
+    "Red",
+    "Black",
+    "Bald"
+]
